390e0b49ce283052537bf8c6fe72db6da2c47e26
Mohammed alzoubi <alzoubi.mohammed94@gmail.com>
2023-08-27 21:50:20 -0700

    More screen • Shows the stored user details • Logout button which clears the session and the user details • handle change language

--- Mostpopular/app/src/main/java/com/deloitte/mostpopular/ui/home/more/MoreFragment.kt
+++ Mostpopular/app/src/main/java/com/deloitte/mostpopular/ui/home/more/MoreFragment.kt
@@ -1,13 +1,30 @@
 package com.deloitte.mostpopular.ui.home.more
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.deloitte.mostpopular.data.model.User
 import com.deloitte.mostpopular.databinding.FragmentMoreBinding
+import com.deloitte.mostpopular.ui.authentication.AuthActivity
+import com.deloitte.mostpopular.ui.home.more.MoreViewModel.MoreViewAction.ChangeLanguage
+import com.deloitte.mostpopular.ui.home.more.MoreViewModel.MoreViewAction.LogoutClicked
+import com.deloitte.mostpopular.ui.home.more.MoreViewModel.MoreViewEvent.*
+import com.deloitte.mostpopular.ui.home.more.MoreViewModel.MoreViewState.*
+import com.deloitte.mostpopular.ui.splash.RoutingActivity
+import com.deloitte.mostpopular.ui.util.extension.setOnSingleClickListener
+import com.deloitte.mostpopular.ui.util.extension.showToast
+import com.deloitte.mostpopular.ui.util.extension.visibility
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MoreFragment : Fragment() {
@@ -39,13 +56,74 @@ class MoreFragment : Fragment() {
     }
 
     private fun initObserveViewModel() {
-        TODO()
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    viewModel.uiEvent.collect {
+                        when (it) {
+                            GoToAuthActivity -> openAuthActivity()
+                            is ChangeLanguageAndResetApp -> changeLanguage(it.language)
+                            is ShowToast -> showToast(it.message)
+                        }
+                    }
+                }
+                launch {
+                    viewModel.uiState.collect {
+                        showProgress(it is Loading)
+                        when (it) {
+                            is Success -> handleSuccess(it.user)
+                            else -> {}
+                        }
+                    }
+                }
+            }
+        }
     }
 
 
     private fun initListener() {
-       TODO()
+        binding.textViewLogout.setOnSingleClickListener {
+            viewModel.processUiAction(LogoutClicked)
+        }
+        binding.textViewLanguage.setOnSingleClickListener {
+           val tag= getLanguageTag()
+            viewModel.processUiAction(ChangeLanguage(tag))
+
+        }
     }
 
+    private fun showProgress(show: Boolean) {
+        binding.progress.root.visibility(show)
+    }
 
+    private fun handleSuccess(user: User) {
+
+        binding.run {
+            textViewName.text = user.fullName
+            textViewEmail.text = user.email
+            textViewNationalId.text = user.nationalId
+            textViewPhone.text = user.phoneNumber
+            textViewDate.text = user.dateOfBirth
+        }
+
+    }
+
+    private fun openAuthActivity() {
+        val intent = Intent(requireActivity(), AuthActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+    }
+
+    private fun changeLanguage(language: String) {
+        val appLocale: LocaleListCompat = LocaleListCompat.forLanguageTags(language)
+        AppCompatDelegate.setApplicationLocales(appLocale)
+        val intent = Intent(requireActivity(), RoutingActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+
+    }
+
+    private fun getLanguageTag():String{
+       return AppCompatDelegate.getApplicationLocales().toLanguageTags()
+    }
 }
--- Mostpopular/app/src/main/java/com/deloitte/mostpopular/ui/home/more/MoreViewModel.kt
+++ Mostpopular/app/src/main/java/com/deloitte/mostpopular/ui/home/more/MoreViewModel.kt
@@ -1,11 +1,105 @@
 package com.deloitte.mostpopular.ui.home.more
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.deloitte.mostpopular.data.model.User
+import com.deloitte.mostpopular.data.network.Resource
+import com.deloitte.mostpopular.domain.usecase.GetUserInfoUseCase
+import com.deloitte.mostpopular.domain.usecase.LogoutUseCase
+import com.deloitte.mostpopular.ui.home.more.MoreViewModel.MoreViewEvent.*
+import com.deloitte.mostpopular.ui.home.more.MoreViewModel.MoreViewState.*
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MoreViewModel @Inject constructor() : ViewModel() {
+class MoreViewModel @Inject constructor(
+    private val getUserInfoUseCase: GetUserInfoUseCase,
+    private val logoutUseCase: LogoutUseCase
+) : ViewModel() {
 
+    private val _uiEvent = Channel<MoreViewEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+    private val _uiState = MutableStateFlow<MoreViewState>(Idle)
+    val uiState = _uiState.asStateFlow()
+
+
+    init {
+        getUserInfo()
+    }
+
+    fun processUiAction(action: MoreViewAction) {
+        when (action) {
+            is MoreViewAction.ChangeLanguage -> onLanguageChange(action.currentLanguageTag)
+            MoreViewAction.LogoutClicked -> logout()
+        }
+    }
+
+    private fun getUserInfo() {
+
+        viewModelScope.launch {
+            getUserInfoUseCase.execute().collect {
+                when (it) {
+                    is Resource.Error -> _uiEvent.send(ShowToast(it.throwable.message))
+                    Resource.Loading -> _uiState.emit(Loading)
+                    is Resource.Success -> _uiState.emit(Success(it.data))
+                }
+            }
+        }
+
+    }
+
+    private fun logout() {
+        viewModelScope.launch {
+            logoutUseCase.execute().collect {
+                when (it) {
+                    is Resource.Error -> _uiEvent.send(ShowToast(it.throwable.message))
+                    Resource.Loading -> _uiState.emit(Loading)
+                    is Resource.Success -> _uiEvent.send(GoToAuthActivity)
+                }
+            }
+        }
+    }
+
+
+    private fun onLanguageChange(currentLanguageTag: String) {
+
+        viewModelScope.launch {
+            val result = if (currentLanguageTag == ARABIC_LANGUAGE_TAG) {
+                ENGLISH_LANGUAGE_TAG
+            } else ARABIC_LANGUAGE_TAG
+
+            _uiEvent.send(ChangeLanguageAndResetApp(result))
+        }
+    }
+
+    sealed class MoreViewEvent() {
+        data class ShowToast(val message: String?) : MoreViewEvent()
+        data class ChangeLanguageAndResetApp(val language: String) : MoreViewEvent()
+        data object GoToAuthActivity : MoreViewEvent()
+
+    }
+
+    sealed class MoreViewState {
+        data object Idle : MoreViewState()
+
+        data class Success(val user: User) : MoreViewState()
+        data object Loading : MoreViewState()
+
+    }
+
+    sealed class MoreViewAction {
+        data object LogoutClicked : MoreViewAction()
+        data class ChangeLanguage(val currentLanguageTag: String) : MoreViewAction()
+    }
+
+    companion object {
+        const val ARABIC_LANGUAGE_TAG = "ar"
+        const val ENGLISH_LANGUAGE_TAG = "en"
+    }
 
 }
